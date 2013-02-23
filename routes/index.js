8e36a1a050fddb15b46703f7e70edd283a6442f3
vinnie <i@vinnie.me>
2013-02-22 23:40:45 -0500

    content update

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@
  */
 
 exports.index = function(req, res){
+	var host = req.headers.host;
 	res.render('index', { 
 		title: 'Jax',
 		tagline: 'Feed me XML and I\'ll spit out JSON',
-		hostname: req.headers.host
+		sampleLink: '/convert?xml=http%3A%2F%2F' + host + '%2Fsamples%2Fbooks.xml'
+		hostname: host
 	});
 };

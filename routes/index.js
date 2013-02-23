25d66e1acfaa05c3789eb48ca989eea4a9fefab4
vinnie <i@vinnie.me>
2013-02-22 23:43:33 -0500

    Fixed syntax error.

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ exports.index = function(req, res){
 	res.render('index', { 
 		title: 'Jax',
 		tagline: 'Feed me XML and I\'ll spit out JSON',
-		sampleLink: '/convert?xml=http%3A%2F%2F' + host + '%2Fsamples%2Fbooks.xml'
+		sampleLink: '/convert?xml=http%3A%2F%2F' + host + '%2Fsamples%2Fbooks.xml',
 		hostname: host
 	});
 };

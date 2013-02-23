bbcb05f8c0fbc4074e183a1b8ecd0469dd06bd98
vinnie <i@vinnie.me>
2013-02-22 22:53:29 -0500

    set CORS on the correct resource

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.all('/', function(req, res, next) {
+app.all('/convert', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();

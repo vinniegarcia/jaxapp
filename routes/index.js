d2322ae2de8b4e81e85acbc05fe3b6cd1b602aba
vinnie <i@vinnie.me>
2013-02-22 23:15:38 -0500

    Updates to layout, better separation of concerns.

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,9 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Jax' });
+	res.render('index', { 
+		title: 'Jax',
+		tagline: 'Feed me XML and I\'ll spit out JSON',
+		hostname: req.headers.host
+	});
 };
